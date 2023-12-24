7e27d487a7ce0945ca5dd3b56709365f41001d11
ksen <ksmatievskaya@gmail.com>
2023-12-24 14:13:40 +0300

    created project, added postcss

---
+++ src/components/CardsList/CardsList.tsx
@@ -0,0 +1,11 @@
+import CardItem from "../CardItem/CardItem"
+
+function CardsList() {
+    return(
+        <div className="cards-list">
+            <CardItem/>
+        </div>
+    )
+}
+
+export default CardsList;
