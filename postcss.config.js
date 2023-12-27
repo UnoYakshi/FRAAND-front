60c1c513bac1f0ed172f405de80f708eac34c5d0
ksen <ksmatievskaya@gmail.com>
2023-12-27 15:11:51 +0400

    added font localy, added css variables, removed web-vitals and other small fixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
 module.exports = {
     plugins: [
         require('postcss-import'),
-        // require('postcss-preset-env')({
-        //     stage: 1,
-        // }),
         require('postcss-nested'),
         require('autoprefixer'),
-        // require('postcss-normalize'),
+        require('postcss-css-variables')
     ],
 };
