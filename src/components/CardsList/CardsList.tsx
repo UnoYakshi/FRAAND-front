761ded1884443b58bfccb1feb0702303db05597d
ksen <ksmatievskaya@gmail.com>
2024-01-27 14:24:20 +0400

    added pagination loading from api

--- src/components/CardsList/CardsList.tsx
+++ src/components/CardsList/CardsList.tsx
@@ -12,52 +12,50 @@ interface Item {
 function CardsList() {
   const { loading, error, getAllItems } = useApiService();
   const [itemList, setItemList] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 3; // You can adjust the number of items per page as needed
+  const [itemsCount, setItemsCount] = useState(4);
+  const [hasMoreItems, setHasMoreItems] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const data = await getAllItems(itemsCount);
+      setItemList(data);
+      if(data.length === itemList.length) {
+        setHasMoreItems(false);
+      }
+      console.log(hasMoreItems)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await getAllItems();
-        setItemList(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const handleMore = () => {
+    setItemsCount((count) => count + 4);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [itemsCount])
+
   function renderItemsList(arr: Item[]) {
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const slicedItems = arr.slice(startIndex, endIndex);
     return (
       <ul className="cards-list">
-        {slicedItems.map((item) => (
+        {arr.map((item) => (
           <CardItem key={item.id} name={item.name} description={item.description} />
         ))}
       </ul>
     );
   }
 
-  const totalPages = Math.ceil(itemList.length / itemsPerPage);
-
-  const handlePageChange = (newPage: number) => {
-    setCurrentPage(newPage);
-    window.scrollTo({top: 0, behavior: 'smooth'});
-  };
 
   return (
     <div>
       {renderItemsList(itemList)}
       <div className='cards-list__pagination'>
-        <button className='cards-list__pagination_btn cards-list__pagination_prev' onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>
-         
-        </button>
-        <span className='cards-list__pagination_count'> Page {currentPage} of {totalPages} </span>
-        <button className='cards-list__pagination_btn cards-list__pagination_next' onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>
-        </button>
+        <button className='cards-list__more' onClick={handleMore} disabled={loading || !hasMoreItems}>{loading ? 'Loading...' : 'Load more'}</button>
       </div>
     </div>
   );
